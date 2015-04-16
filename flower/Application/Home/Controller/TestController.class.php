5160d5cf897c1ea1092d56b2bd1bd12d6df944ff
zxc7928932 <howdoesmylove@qq.com>
2015-04-16 21:58:16 +0800

    @刘海洋 我已经ueditor后端修改 在test控制器里有

---
+++ flower/Application/Home/Controller/TestController.class.php
@@ -0,0 +1,16 @@
+<?php
+// +----------------------------------------------------------------------
+// | Flori Website
+// +----------------------------------------------------------------------
+// | Copyright (c) 2015 .... All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: 尹俊民 <vrforyjm@gmail.com>
+// +----------------------------------------------------------------------
+
+namespace Home\Controller;
+use Think\Controller;
+class TestController extends Controller {
+    public function index(){
+        $this->display();
+    }
+}
